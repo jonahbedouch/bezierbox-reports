0045146c544a4605f7d9708e8f3cfb5dcffa5e0f
Jonah Alex Bedouch <jonahbedouch@gmail.com>
2025-04-03 20:54:32 -0700

    Update Footer Attribution

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className="flex h-7 w-max mx-auto items-center justify-center mt-md mb-2xs">
       <small className="font-lato text-base oldstyle-nums mr-4">
-        &#127279; 2025 Jonah Bedouch and Brandon Wong
+        &#127279; 2025 BezierBox Team
       </small>
       <ThemeSelector />
     </footer>
